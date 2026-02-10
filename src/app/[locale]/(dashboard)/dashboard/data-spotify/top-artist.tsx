a2b4e81f61b9de3bc269c8f6c4c10913bde93045
Gianella Carla Lastra <gianellalastra4@gmail.com>
2026-02-10 17:02:17 -0300

    feat: implemento React Query para manejo de datos en TopArtists

--- src/app/[locale]/(dashboard)/dashboard/data-spotify/top-artist.tsx
+++ src/app/[locale]/(dashboard)/dashboard/data-spotify/top-artist.tsx
@@ -2,21 +2,23 @@
 import { CardArtist } from "@/src/app/_components/cardArtist"
 import { useTopArtists } from "@/src/hooks/useTopArtists";
 import  Loading  from "@/src/app/_components/loading";
+import { Artist } from "@/src/types/spotify";
 
 
 type TypeTimeRange = {
   timeRange: string;
 }
 export function TopArtist(timeRange: TypeTimeRange) {
-  const { artists, loading, error } = useTopArtists(timeRange);
-  if (loading) return <Loading />;
-  if (error) return <p>{error}</p>;
+  const { data: artists, isLoading, isError, error } = useTopArtists(timeRange);
+
+  if (isLoading) return <Loading />;
+  if (isError) return <p>{(error as Error).message}</p>;
   return (
     <>
       <div className="flex flex-col items-center justify-center">
         <h1 className="text-2xl font-bold mb-6">Tus artistas recientemente escuchados</h1>
         <div className=" grid grid-cols-1 sm:grid-cols-2 gap-4 w-full max-w-4xl">
-          {artists.map((artist) => (
+          {artists?.map((artist: Artist) => (
             <CardArtist key={artist.id} artist={artist} />
           ))}
         </div>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,16 @@
 "use client";
 import "@/src/app/globals.css";
 import { SessionProvider } from "next-auth/react";
+import { Providers } from "./provider"
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="es">
       <body>
         <SessionProvider>
-          {children}
+          <Providers>
+            {children}
+          </Providers>
         </SessionProvider>
       </body>
     </html>
