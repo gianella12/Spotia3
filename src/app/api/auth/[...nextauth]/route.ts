bf9b596f270c0e134f5e560bd430ac241d6ecd90
Gianella Carla Lastra <gianellalastra4@gmail.com>
2026-02-18 15:59:10 -0300

    lógica de refresh automático del token de Spotify en jwt callback

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -14,18 +14,57 @@ export const authOptions: NextAuthOptions = {
       },
     }),
   ],
-pages:{error: "/auth/error"},
+  pages: { error: "/auth/error" },
   callbacks: {
     async jwt({ token, account }) {
-      if (account?.access_token) {
-        token.accessToken = account.access_token;
+
+      if (account) {
+        return {
+          ...token,
+          accessToken: account.access_token,
+          expires_at: account.expires_at,
+          refreshToken: account.refresh_token,
+        }
+      } else if (Date.now() < (token.expires_at as number) * 1000) {
+     
+        return token
+      } else {
+
+        if (!token.refreshToken) throw new TypeError("Missing refresh_token")
+
+        try {
+          const response = await fetch("https://accounts.spotify.com/api/token", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/x-www-form-urlencoded",
+              "Authorization": "Basic " + Buffer.from(
+                `${process.env.SPOTIFY_CLIENT_ID}:${process.env.SPOTIFY_CLIENT_SECRET}`
+              ).toString("base64"),
+            },
+            body: new URLSearchParams({
+              grant_type: "refresh_token",
+              refresh_token: token.refreshToken as string,
+            }),
+          })
+          const newTokens = await response.json()
+          if (!response.ok) throw newTokens
+          return {
+            ...token,
+            accessToken: newTokens.access_token,
+            expires_at: Math.floor(Date.now() / 1000 + newTokens.expires_in),
+            refreshToken: newTokens.refresh_token ?? token.refreshToken,
+          }
+        } catch (error) {
+          console.error("Error refreshing access_token", error)
+          return { ...token, error: "RefreshTokenError" }
+        }
       }
-      return token;
     },
 
     async session({ session, token }) {
-      session.accessToken = token.accessToken as string;
-      return session;
+      session.accessToken = token.accessToken as string
+      session.error = token.error as string
+      return session
     },
   },
 };
