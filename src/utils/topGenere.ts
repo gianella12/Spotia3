6337330dbe873dad8a5cf144c066cc8b4f01ffa3
Gianella Carla Lastra <gianellalastra4@gmail.com>
2026-02-20 00:46:35 -0300

    limpio el codigo

--- src/utils/topGenere.ts
+++ src/utils/topGenere.ts
@@ -21,21 +21,5 @@ export const topGenres = (artists: { genres: string[] }[]) => {
     count,
     percentage: parseFloat(((count / totalTop5) * 100).toFixed(1)),
   }))
-//   const totalArtists = artists.length;
-
-//  const genrePercentage = Object.entries(genreCount)
-//     .map(([genre, count]) => ({
-//       genre,
-//       count, // Cuántos artistas tienen este género
-//       percentage: parseFloat(((count / totalArtists) * 100).toFixed(1)),
-//     }))
-//     .sort((a, b) => b.percentage - a.percentage);
-  // const sumadosGeneros = Object.values(genreCount);
-  // const total = sumadosGeneros.reduce((a, b) => a + b, 0);
-
-  // const genrePercentage = Object.entries(genreCount).map(([genre, count]) => ({
-  //   genre,
-  //   percentage: ((count / total) * 100).toFixed(),
-  // }));
   return genreData;
 };
